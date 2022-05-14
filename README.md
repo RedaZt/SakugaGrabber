fd9903a701881360dd9c6eba7ed35b306dbf8288
RedaZt <redazitouni245@gmail.com>
2022-05-14 14:14:06 +0100

    Fix downloading error #1

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # SakugaGrabber
 A python script to download media from [www.sakugabooru.com](https://www.sakugabooru.com).
-This script helps you download Posts, Pools, and even bulk download posts that contain given tags.
+This script helps you download Posts, Pools, and even bulk download posts that contain given tags, all you need to do is copy the link and paste it and it'll do the rest.
 
 ## Requirements
   * [Python 3.4+](https://www.python.org/downloads/)
-  * bs4
+  * [bs4](https://pypi.python.org/pypi/beautifulsoup4)
 
 ## Usage
-* Download the sakugagrabber.py 
-* Double click to run the script.
+* Download the latest "sakugagrabber.py" [here](https://github.com/RedaZt/SakugaGrabber/releases).
+* Run the script.
+* Paste you link and done.
 
 ### Example usage
 ```
 Paste your link here : https://www.sakugabooru.com/post/show/142283
-Downloaded : jujutsu_kaisen By kenichi kutsuna & myoun (142283).mp4
+DOWNLOADING : jujutsu_kaisen By kenichi kutsuna & myoun (142283).mp4
+SUCCESS : jujutsu_kaisen By kenichi kutsuna & myoun (142283).mp4
+DONE !!
 ```
--- sakugagrabber.py
+++ sakugagrabber.py
@@ -1,123 +1,104 @@
-import os, urllib3
+import os, re, urllib3
 from bs4 import BeautifulSoup as bs
+from typing import List
 
-dict_art = { "+" : "", "!" : "", "?" : "", "%" : "", "*" : "", "/" : "", "#" : "", "\\": "", "&" : "and", ":" : "", "_" : " "}
-dict = { "+" : "", "!" : "", "?" : "", "%" : "", "*" : "", "/" : "", "#" : "", "\\": "", "&" : "and", ":" : ""}
+specialCharacters = {'?' : '', '"' : '', '*' : '', '/' : '', '#' : '', '\\': '', ':' : '', '_' : ' ', '|' : '', '<' : '', '>' : ''}
+mainFolder = "Sakugabooru Downloads"
 http = urllib3.PoolManager()
 
-def Filename(res, id):
+def search(infos : List, className : str) -> List:
+    lst = []
+    for element in infos:
+        if element["class"] == [className]:
+            childElement = element.findChildren('a')
+            for target in childElement:
+                if target.text != '?':
+                    lst += [target.text]
+    return lst
+
+def namingFiles(res, id : str) -> str:
     soup = bs(res.data,"html.parser")
-    tags = soup.findAll('ul',id="tag-sidebar")
-    ext = '.' + soup.find('a', id="highres")["href"].split('.')[-1]
-    art_num = 0
-    cpr_num = 0
-    artist = ''
-    copyright = ''
-    for tag in tags:
-        info = tag.findChildren('li')
-        for x in info : 
-            if x["data-type"] == "artist" :
-                if art_num == 0 : 
-                    artist = x["data-name"].translate(str.maketrans(dict_art))
-                    art_num += 1
-                else :
-                    artist += " & " + x["data-name"].translate(str.maketrans(dict_art))
-            if x["data-type"] == "copyright" :
-                if art_num == 0 : 
-                    copyright = x["data-name"].translate(str.maketrans(dict)).strip()
-                    cpr_num += 1
-                else :
-                    copyright += " " + x["data-name"].translate(str.maketrans(dict)).strip()
-    file_name = (str(copyright) + " By " + artist + " (" + id + ')' + ext).strip()
-    return file_name
+    data = soup.find('ul', id="tag-sidebar")
+    infos = data.findChildren('li')
+    ext = soup.find('a', id="highres")["href"].split('.')[-1]
 
-def postGrabber(id):
-    link = "https://www.sakugabooru.com/post/show/"+id
+    artists = ' & '.join(search(infos, "tag-type-artist"))
+    copyrights = ' & '.join(search(infos, "tag-type-copyright"))
+
+    fileName = f"{copyrights} By {artists} ({id}).{ext}".translate(str.maketrans(specialCharacters))
+
+    return fileName
+
+def postGrabber(folder : str, id : str):
+    link = f"https://www.sakugabooru.com/post/show/{id}"
     res = http.request('GET', link)
-    media_link = bs(res.data, "html.parser").find('a', id="highres")["href"]
+    mediaLink = bs(res.data, "html.parser").find('a', id="highres")["href"]
 
-    media = Filename(res, id)
+    media = namingFiles(res, id)
+    print(f"DOWNLOADING : {media}")
 
-    if os.path.exists(media):
-        pass
-    else : 
-        with open(media, 'wb') as targetfile :
-            dt = http.request('GET', media_link)
-            targetfile.write(dt.data)
-        print("Downloaded :",media)
-
-def idDownloader(link):
-    id = link.split('/')[-1]
-
-    folder = "Sakugabooru Downloads" 
     if not os.path.exists(folder):
-        os.mkdir(folder)
-    os.chdir(folder)
+        os.makedirs(folder)
 
-    postGrabber(id)
+    filepath = f"{folder}/{media}"
 
-def bulkDownloader(link):
-    limit = "&limit=100"
-    page = "&page="
-    page_number = 1
+    with open(filepath, 'wb+') as targetfile:
+        dt = http.request('GET', mediaLink)
+        targetfile.write(dt.data)
+    print(f"SUCCESS : {media}")
 
-    res = http.request('GET', link)
-    soup = bs(res.data,"xml")
-    tags = soup.find("posts")
-    posts_count = int(tags["count"])
+def getId(link : str) -> str:
+    return re.search(r"(\d+)", link).group(1)
 
-    print(posts_count,"Posts found.")
+def idDownloader(link : str):
+    id = getId(link)
+    postGrabber(mainFolder, id)
+
+def bulkDownloader(link : str):
+    tags = link.split("=")[-1] 
+    tagsLink = f"https://www.sakugabooru.com/post.xml?tags={tags}"
+    pageNumber = 1
+
+    res = http.request('GET', tagsLink)
+    soup = bs(res.data, "lxml")
+    posts = soup.find("posts")
+    postCount = int(posts["count"])
+
+    print(f"{postCount} posts found.")
     print("Downloading ...")
 
-    while posts_count > 0 :
-        print("requesting")
-        url = link + limit + page + str(page_number)
+    while postCount > 0:
+        url = f"{tagsLink}&limit=100&page={pageNumber}"
 
-        res2 = http.request('GET', url)
-        soup = bs(res2.data,"xml")
-        tags = soup.find("posts")
-        posts = tags.find_all("post")
-
-        folder = "sakugabooru_downloads" 
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        os.chdir(folder)
+        res = http.request('GET', url)
+        soup = bs(res.data, "lxml")
+        posts = soup.findAll("post")
 
         for post in posts:
-            postGrabber(post["id"])
+            postGrabber(mainFolder, post["id"])
         
-        posts_count -= 100
-        page_number += 1
-        
+        postCount -= 100
+        pageNumber += 1
 
-def poolDownloader(link):
+def poolDownloader(link : str):
     res = http.request('GET', link)
     soup = bs(res.data, "html.parser")
     posts = soup.find_all('span', attrs={"class" : "plid"})
-    pool_title = soup.find("div", attrs={"id" : "pool-show"}).find("h4").text.split(':')[-1].strip()
+    poolTitle = soup.find("div", attrs={"id" : "pool-show"}).find("h4").text.split(':')[-1].strip()
     
-    folder = "sakugabooru_downloads" 
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-    os.chdir(folder)
-    
-    if not os.path.exists(pool_title):
-        os.mkdir(pool_title)
-    os.chdir(pool_title)
+    folder = f"{mainFolder}/{poolTitle}"
 
     for post in posts:
-        postGrabber(post.text.split('/')[-1])
-
+        postGrabber(folder, getId(post.text))
 
 if __name__ == "__main__":
-    link = input("Paste your link here : ").strip()                       
+    link = input("Paste your link here : ").strip()                   
 
-    if "tags=" in link :
-        link_F = "https://www.sakugabooru.com/post.xml?tags=" + link.split("=")[-1] 
-        bulkDownloader(link_F)
-
-    elif "pool" in link : 
+    if "tags=" in link:
+        bulkDownloader(link)
+    elif "pool" in link: 
         poolDownloader(link)
-
     else : 
         idDownloader(link)
+
+    print("DONE !!")
